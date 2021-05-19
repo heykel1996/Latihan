4b9810486821b35fd48cd717553365b0099eb13a
heykel1996 <82283376+heykel1996@users.noreply.github.com>
2021-05-19 07:58:40 +0700

    latihan 11.1.php

---
+++ latihan 11.1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<title>Belajar cara memasukkan kode PHP</title>
+</head>
+<body>
+<p>Kalimat ini dibuat menggunakan HTML saja </p>
+<?php 
+echo "<p>Kalimat ini dibuat menggunakan mode PHP style</p>"; 
+?>
+<? 
+echo "<p>Kalimat ini dibuat menggunakan mode SMGL style </p>"; 
+?>
+<%
+echo "<p>Kalimat ini dibuat menggunakan mode ASP Style </p>"; 
+%>
+<script language="php">
+echo "<p>Kalimat ini dibuat menggunakan mode Script Style </p>"; 
+</script>
+<?=" <p>Kalimat ini dibuat menggunakan mode Instan style </p>"; ?>
+</body>
+</html>
